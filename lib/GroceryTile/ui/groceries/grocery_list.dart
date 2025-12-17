60eccab0e0fcdf8523f4ef856eb25d2bb4417e0b
vathanakphy <vathanakphy@gmail.com>
2025-12-17 15:55:43 +0700

    challenge before lap

---
+++ lib/GroceryTile/ui/groceries/grocery_list.dart
@@ -0,0 +1,59 @@
+import 'package:basic/GroceryTile/models/grocery.dart';
+import 'package:flutter/material.dart';
+import '../../data/mock_grocery_repository.dart';
+
+class GroceryList extends StatefulWidget {
+  const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  void onCreate() {
+    // TODO-4 - Navigate to the form screen using the Navigator push
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = const Center(child: Text('No items added yet.'));
+
+    if (dummyGroceryItems.isNotEmpty) {
+      // TODO-1 - Display groceries with an Item builder and  LIst Tile
+      content = ListView.builder(
+        itemCount: dummyGroceryItems.length,
+        itemBuilder: (ctx, index) {
+            return GroceryTile(grocery: dummyGroceryItems[index]);
+        },
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Groceries'),
+        actions: [IconButton(onPressed: () => {}, icon: const Icon(Icons.add))],
+      ),
+      body: content,
+    );
+  }
+}
+
+class GroceryTile extends StatelessWidget {
+  const GroceryTile({super.key, required this.grocery});
+
+  final Grocery grocery;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO-2 - Display groceries with an Item builder and  LIst Tile
+    return ListTile(
+      leading: Container(
+        width: 15,
+        height: 15,
+        color: grocery.category.color,
+      ),
+      title: Text(grocery.name),
+      trailing: Text('${grocery.quantity}'),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,99 @@
-import 'package:basic/ExpenceApp/main.dart';
-import 'package:basic/ExpenceApp/models/expense.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  
-  Expense.listExpense.add(Expense("Flutter", 100, DateTime.now(), ExpenseType.food));
-  Expense.listExpense.add(Expense("Flutter", 100, DateTime.now(), ExpenseType.food));
-  Expense.listExpense.add(Expense("Flutter", 100, DateTime.now(), ExpenseType.food));
+  runApp(const MyApp());
+}
 
-  runApp(const ExpenseApp());
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(title: 'Navigation Demo', home: HomeScreen());
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Screen')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('This is the Home Screen'),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SecondScreen()),
+                );
+              },
+              child: const Text('Go to Second Screen'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Second Screen')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('This is the Second Screen'),
+
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Back to Home Screen'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const ThirdScreen()));
+              },
+              child: const Text('Go to Third Screen'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ThirdScreen extends StatelessWidget {
+  const ThirdScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Third Screen')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('This is the Third Screen'),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Back to Second Screen'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
